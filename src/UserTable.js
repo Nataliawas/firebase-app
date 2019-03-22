604e0ead53a1b4f78ec1e9e1feb2ddd39f2ea12c
Nataliawas <nataliawas@wp.pl>
2019-03-22 05:28:45 +0100

    table added

--- src/UserTable.js
+++ src/UserTable.js
@@ -1,7 +1,26 @@
 import React from 'react'
-import { Container, Label, Table } from 'semantic-ui-react'
+import { Container, Table } from 'semantic-ui-react'
+import { db } from './firebase'
 
 class UserTable extends React.Component {
+    state = {
+        users: []
+    }
+
+    componentDidMount() {
+        db.ref('users/').on("value", snapshot => {
+          const users = []
+          Object.entries(snapshot.val()).forEach(elem => {
+            const user = {
+              id: elem[0],
+              ...elem[1]
+            }
+            users.push(user)
+          })
+          this.setState({ users })
+        })
+      }
+
 
     render() {
         return (
@@ -16,13 +35,13 @@ class UserTable extends React.Component {
                     </Table.Header>
 
                     <Table.Body>
-                        <Table.Row>
-                            <Table.Cell>
-                                <Label ribbon>cell</Label>
-                            </Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                        </Table.Row>
+                        {this.state.users.map(user => (
+                            <Table.Row key={user.id}>
+                                <Table.Cell>{user.firstName}</Table.Cell>
+                                <Table.Cell>{user.lastName}</Table.Cell>
+                                <Table.Cell >{user.age}</Table.Cell>
+                            </Table.Row>
+                        ))}
                     </Table.Body>
                 </Table >
             </Container>
